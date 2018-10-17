4a7984228ee503632add03871f4bc7ce92c4082f
nickfreemandesign <nicho.freeman@gmail.com>
2018-10-17 07:18:16 -0400

    initial commit wussup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import { Router, Link } from "@reach/router"
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+          sahdu
+      </div>
+    )
+  }
+}
+
---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractSass = new ExtractTextPlugin({
+  filename: 'styles.css'
+});
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        presets: ['env', 'react', 'stage-2'],
+        plugins: ['transform-decorators-legacy']
+      }
+    }, 
+    {
+      test: /\.*(sass|scss)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        'file-loader',
+      ],
+    }, ]
+  },
+  plugins: [
+    extractSass,
+    new HtmlWebpackPlugin({
+      template: path.join(path.resolve(__dirname, './'), 'index.html'),
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  node: {
+    fs: 'empty'
+  }
+};
